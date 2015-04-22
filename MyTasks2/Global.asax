f617f9dd9febb143d3b04a3cfa2491b6aeddfa98
Andrey Lapin <lapin@skbkontur.ru>
2015-04-22 18:36:13 +0400

    first commit

---
+++ MyTasks2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyTasks2.MvcApplication" Language="C#" %>
---
+++ MyTasks2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyTasks2.Startup))]
+namespace MyTasks2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
